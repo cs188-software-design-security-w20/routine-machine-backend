4df29ec52e1785558a7fb1504c09018b826f29d8
Seoyoon <seoyoon.jin221@gmail.com>
2021-05-18 23:50:30 -0700

    deployment environment setup

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -4,11 +4,12 @@ config();
 
 export interface EnvConfig {
   db: {
-    name: string;
-    password: string;
-    user: string;
-    host: string;
-    port: number;
+    db_url: string;
+    // name: string;
+    // password: string;
+    // user: string;
+    // host: string;
+    // port: number;
   };
   serverPort: number;
   nodeEnv: string;
@@ -16,33 +17,37 @@ export interface EnvConfig {
 }
 
 const defaultServerPort = 8000;
-const defaultDBPort = 5432;
+// const defaultDBPort = 5432;
 
 const {
-  DB_NAME,
-  DB_PASS,
-  DB_USER,
-  DB_HOST,
-  DB_PORT,
+  // DB_NAME,
+  // DB_PASS,
+  // DB_USER,
+  // DB_HOST,
+  // DB_PORT,
+  DATABASE_URL,
   PORT,
   NODE_ENV,
   PROJECT_ID,
 } = process.env;
 
-if (!DB_NAME) throw Error('"DB_NAME" missing from .env file');
-if (!DB_PASS) throw Error('"DB_PASS" missing from the .env file');
-if (!DB_USER) throw Error('"DB_USER" missing from the .env file');
-if (!DB_HOST) throw Error('"DB_HOST" missing from the .env file. For local testing, this should be set to "localhost"');
+// if (!DB_NAME) throw Error('"DB_NAME" missing from .env file');
+// if (!DB_PASS) throw Error('"DB_PASS" missing from the .env file');
+// if (!DB_USER) throw Error('"DB_USER" missing from the .env file');
+// if (!DB_HOST) throw Error('"DB_HOST" missing from the .env file. For local testing, this should be set to "localhost"');
+
+if (!DATABASE_URL) throw Error('"DATABASE_URL" missing from the .env file');
 if (!NODE_ENV) throw Error('"NODE_ENV" missing from the .env file');
 if (!PROJECT_ID) throw Error('"PROJECT_ID" missing from the .env file. This is associated to a firebase project at https://console.firebase.google.com');
 
 const env: EnvConfig = {
   db: {
-    name: DB_NAME,
-    password: DB_PASS,
-    host: DB_HOST,
-    user: DB_USER,
-    port: DB_PORT ? Number(DB_PORT) : defaultDBPort,
+    db_url: DATABASE_URL,
+    // name: DB_NAME,
+    // password: DB_PASS,
+    // host: DB_HOST,
+    // user: DB_USER,
+    // port: DB_PORT ? Number(DB_PORT) : defaultDBPort,
   },
   serverPort: PORT ? Number(PORT) : defaultServerPort,
   nodeEnv: NODE_ENV,
