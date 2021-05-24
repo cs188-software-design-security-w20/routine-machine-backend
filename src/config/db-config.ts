b67e4ca1cfb24d63cae57cd5652c9d9b3bc65a96
Seoyoon <seoyoon.jin221@gmail.com>
2021-05-24 15:06:01 -0700

    refactored to support development and production

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -1,7 +1,5 @@
 import { Transaction } from 'sequelize';
 import type { SequelizeOptions } from 'sequelize-typescript';
-import parseDbUrl from 'parse-database-url';
-
 import env from './env-config';
 
 export type DBEnvironment = 'development' | 'test' | 'production';
@@ -10,74 +8,33 @@ export type SequelizeOptionEnvironments = {
   [key in DBEnvironment]: SequelizeOptions
 };
 
-const dbConfig = parseDbUrl(env.db.db_url);
-
-// const dbOptions: SequelizeOptionEnvironments = {
-//   development: {
-//     username: env.db.user,
-//     password: env.db.password,
-//     database: env.db.name,
-//     host: env.db.host,
-//     port: env.db.port,
-//     dialect: 'postgres',
-//     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-//     logging: false
-//   },
-//   test: {
-//     username: env.db.user,
-//     password: env.db.password,
-//     database: env.db.name,
-//     host: env.db.host,
-//     port: env.db.port,
-//     dialect: 'postgres',
-//     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-//     logging: false
-//   },
-//   production: {
-//     username: env.db.user,
-//     password: env.db.password,
-//     database: env.db.name,
-//     host: env.db.host,
-//     port: env.db.port,
-//     dialect: 'postgres',
-//     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-//     logging: false,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   },
-// };
-
 const dbOptions: SequelizeOptionEnvironments = {
   development: {
-    username: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.name,
-    host: dbConfig.host,
-    port: dbConfig.port,
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name,
+    host: env.db.host,
+    port: env.db.port,
     dialect: 'postgres',
     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
     logging: false,
   },
   test: {
-    username: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.name,
-    host: dbConfig.host,
-    port: dbConfig.port,
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name,
+    host: env.db.host,
+    port: env.db.port,
     dialect: 'postgres',
     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
     logging: false,
   },
   production: {
-    username: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.name,
-    host: dbConfig.host,
-    port: dbConfig.port,
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name,
+    host: env.db.host,
+    port: env.db.port,
     dialect: 'postgres',
     isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
     logging: false,
--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -1,15 +1,16 @@
 import { config } from 'dotenv';
+// @ts-ignore
+import parseDbUrl from 'parse-database-url';
 
 config();
 
 export interface EnvConfig {
   db: {
-    db_url: string;
-    // name: string;
-    // password: string;
-    // user: string;
-    // host: string;
-    // port: number;
+    name: string;
+    password: string;
+    user: string;
+    host: string;
+    port: number;
   };
   serverPort: number;
   nodeEnv: string;
@@ -17,41 +18,67 @@ export interface EnvConfig {
 }
 
 const defaultServerPort = 8000;
-// const defaultDBPort = 5432;
+const defaultDBPort = 5432;
 
 const {
-  // DB_NAME,
-  // DB_PASS,
-  // DB_USER,
-  // DB_HOST,
-  // DB_PORT,
-  DATABASE_URL,
-  PORT,
   NODE_ENV,
   PROJECT_ID,
 } = process.env;
 
-// if (!DB_NAME) throw Error('"DB_NAME" missing from .env file');
-// if (!DB_PASS) throw Error('"DB_PASS" missing from the .env file');
-// if (!DB_USER) throw Error('"DB_USER" missing from the .env file');
-// if (!DB_HOST) throw Error('"DB_HOST" missing from the .env file. For local testing, this should be set to "localhost"');
-
-if (!DATABASE_URL) throw Error('"DATABASE_URL" missing from the .env file');
 if (!NODE_ENV) throw Error('"NODE_ENV" missing from the .env file');
 if (!PROJECT_ID) throw Error('"PROJECT_ID" missing from the .env file. This is associated to a firebase project at https://console.firebase.google.com');
+// eslint-disable-next-line import/no-mutable-exports
+let env: EnvConfig;
 
-const env: EnvConfig = {
-  db: {
-    db_url: DATABASE_URL,
-    // name: DB_NAME,
-    // password: DB_PASS,
-    // host: DB_HOST,
-    // user: DB_USER,
-    // port: DB_PORT ? Number(DB_PORT) : defaultDBPort,
-  },
-  serverPort: PORT ? Number(PORT) : defaultServerPort,
-  nodeEnv: NODE_ENV,
-  firebaseProjectID: PROJECT_ID,
-};
+if (NODE_ENV === 'production') {
+  const {
+    DATABASE_URL,
+    PORT,
+  } = process.env;
+
+  if (!DATABASE_URL) throw Error('"DATABASE_URL" missing in the environment');
+  const dbConfig = parseDbUrl(DATABASE_URL);
+  env = {
+    db: {
+      name: dbConfig.name,
+      password: dbConfig.password,
+      host: dbConfig.host,
+      user: dbConfig.user,
+      port: dbConfig.port ? Number(dbConfig.port) : defaultDBPort,
+    },
+    serverPort: PORT ? Number(PORT) : defaultServerPort,
+    nodeEnv: NODE_ENV,
+    firebaseProjectID: PROJECT_ID,
+  };
+} else if (NODE_ENV === 'development') {
+  const {
+    DB_NAME,
+    DB_PASS,
+    DB_USER,
+    DB_HOST,
+    DB_PORT,
+    SERVER_PORT,
+  } = process.env;
+  if (!DB_NAME) throw Error('"DB_NAME" missing from .env file');
+  if (!DB_PASS) throw Error('"DB_PASS" missing from the .env file');
+  if (!DB_USER) throw Error('"DB_USER" missing from the .env file');
+  if (!DB_HOST) throw Error('"DB_HOST" missing from the .env file.');
+  if (!DB_PORT) throw Error('"DB_PORT" missing from the .env file.');
+
+  env = {
+    db: {
+      name: DB_NAME,
+      password: DB_PASS,
+      host: DB_HOST,
+      user: DB_USER,
+      port: DB_PORT ? Number(DB_PORT) : defaultDBPort,
+    },
+    serverPort: SERVER_PORT ? Number(SERVER_PORT) : defaultServerPort,
+    nodeEnv: NODE_ENV,
+    firebaseProjectID: PROJECT_ID,
+  };
+} else {
+  throw Error(`NODE_ENV set to ${NODE_ENV}, should be production or development`);
+}
 
 export default env;
