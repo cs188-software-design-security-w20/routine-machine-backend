ea7add1d4340b2ff988dbefdb8b510ffff3d3533
Seoyoon <seoyoon.jin221@gmail.com>
2021-05-28 23:36:53 -0700

    changed public key encryption

--- src/router/challenge.js
+++ src/router/challenge.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import crypto from 'crypto';
+import sshpk from 'sshpk';
 import { getPK } from '../service/user-service';
 
 const challengeRouter = Router();
@@ -15,7 +16,7 @@ challengeRouter.get('/', async (req, res) => {
     const { public_key } = await getPK(id);
     const challengeString = randomString(); // randomly generated string
     const encryptedString = crypto.publicEncrypt(
-      public_key,
+      sshpk.parseKey(public_key, 'pem'),
       Buffer.from(challengeString),
     );
     res.status(200).json({ challengeString, encryptedString });
